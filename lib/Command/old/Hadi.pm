53c779345dcdd5489855d52788514393a8b04e76
robertrendler <rendler@gmail.com>
2025-07-02 19:49:23 +1000

    Old redundant files moved here.

---
+++ lib/Command/old/Hadi.pm
@@ -0,0 +1,57 @@
+package Command::Hadi;
+use feature 'say';
+use utf8;
+
+use Moo;
+use strictures 2;
+use namespace::clean;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(cmd_hadi);
+
+has bot                 => ( is => 'ro' );
+has discord             => ( is => 'lazy', builder => sub { shift->bot->discord } );
+has log                 => ( is => 'lazy', builder => sub { shift->bot->log } );
+has some_random         => ( is => 'lazy', builder => sub { shift->bot->some_random } );
+
+has name                => ( is => 'ro', default => 'Hadi' );
+has access              => ( is => 'ro', default => 0 );
+has description         => ( is => 'ro', default => 'Make the bot say something good' );
+has pattern             => ( is => 'ro', default => '^hadi' );
+has function            => ( is => 'ro', default => sub { \&cmd_hadi } );
+has usage               => ( is => 'ro', default => 'https://bob.rendler.org/en/commands/hadi');
+
+sub cmd_hadi {
+
+    my ($self, $msg) = @_;
+
+    my $discord = $self->discord;
+    my $pattern = $self->pattern;
+ 
+    my $channel = $msg->{'channel_id'};
+    my $author  = $msg->{'author'};
+    my $args    = $msg->{'content'};
+       $args    =~ s/$pattern//i;
+
+    
+    my $replyto = '<@' . $author->{'id'} . '>';
+
+    if ($args =~ /and his girlfriend/i) {
+        $self->some_random->animu('hug')->then(sub {
+            my $json = shift;
+            my $wink = $json->{'image'};
+            $self->discord->send_message($channel, $wink);
+        })->catch(sub{
+            $self->discord->send_message($channel, ":x: Sorry, couldn't find any animu. Try again later!");
+        });
+    } elsif ($args =~ /with his girlfriend/i) {
+        $discord->send_image($channel, {'content' => '', 'name' => 'gf.png', 'path' => "lib/Command/images/gf.png"});
+    } else {
+        $discord->send_message($channel, "Hadi likes anime girls. Fact.");
+        
+    }
+
+    $discord->create_reaction($msg->{'channel_id'}, $msg->{'id'}, "🤖");
+}
+
+1;
---
+++ lib/Command/old/Yahya.pm
@@ -0,0 +1,48 @@
+package Command::Yahya;
+use feature 'say';
+use utf8;
+
+use Moo;
+use strictures 2;
+use namespace::clean;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(cmd_yahya);
+
+has bot                 => ( is => 'ro' );
+has discord             => ( is => 'lazy', builder => sub { shift->bot->discord } );
+has log                 => ( is => 'lazy', builder => sub { shift->bot->log } );
+has some_random         => ( is => 'lazy', builder => sub { shift->bot->some_random } );
+
+has name                => ( is => 'ro', default => 'Yahya' );
+has access              => ( is => 'ro', default => 0 );
+has description         => ( is => 'ro', default => 'Make the bot say something good' );
+has pattern             => ( is => 'ro', default => '^yahya' );
+has function            => ( is => 'ro', default => sub { \&cmd_yahya } );
+has usage               => ( is => 'ro', default => 'https://bob.rendler.org/en/commands/yahya');
+
+sub cmd_yahya {
+
+    my ($self, $msg) = @_;
+
+    my $channel = $msg->{'channel_id'};
+    my $author = $msg->{'author'};
+    my $args = $msg->{'content'};
+
+    my $pattern = $self->pattern;
+    $args =~ s/$pattern//i;
+
+    my $discord = $self->discord;
+    my $replyto = '<@' . $author->{'id'} . '>';
+
+    if ($args =~ /(with|and) his girlfriend/) {
+        $discord->send_image($channel, {'content' => 'Forbidden Love.', 'name' => 'mac.png', 'path' => "lib/Command/images/mac.png"});
+        $discord->create_reaction($msg->{'channel_id'}, $msg->{'id'}, "🤖");
+    } else {
+        $discord->send_image($channel, {'content' => 'The genius behind Apple.', 'name' => 'apple.jpg', 'path' => "lib/Command/images/apple.jpg"});
+        $discord->create_reaction($msg->{'channel_id'}, $msg->{'id'}, "🤖");
+    }
+
+}
+
+1;
