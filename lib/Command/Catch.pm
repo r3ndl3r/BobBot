9ea462ad9bf01f9425a6c660d0b0592811350676
robertrendler <rendler@gmail.com>
2025-07-05 00:24:27 +1000

    Using global DB object

--- lib/Command/Catch.pm
+++ lib/Command/Catch.pm
@@ -7,14 +7,14 @@ use strictures 2;
 use namespace::clean;
 
 use Storable qw(freeze);
-
 use Exporter qw(import);
-use Component::DBI;
+
 our @EXPORT_OK = qw(cmd_catch catch);
 
 has bot                 => ( is => 'ro' );
 has discord             => ( is => 'lazy', builder => sub { shift->bot->discord } );
 has log                 => ( is => 'lazy', builder => sub { shift->bot->log } );
+has db                  => ( is => 'ro', required => 1 );
 has name                => ( is => 'ro', default => '' );
 has access              => ( is => 'ro', default => 0 );
 has usage               => ( is => 'ro', default => '' );
@@ -23,18 +23,14 @@ has pattern             => ( is => 'ro', default => '' );
 has function            => ( is => 'ro', default => sub {} );
 
 has on_message => ( is => 'ro', default =>
-
     sub {
         my $self = shift;
         $self->discord->gw->on('MESSAGE_CREATE' =>     
           
             sub {
                     my ($s, $m) = @_;
-                    my $db  = Component::DBI->new();
-                    my $dbh = $db->{'dbh'};
-
                     my $sql = "INSERT INTO messages (id, content) VALUES(?, ?)";
-                    my $sth = $dbh->prepare($sql);
+                    my $sth = $self->db->dbh->prepare($sql);
 
                     $sth->execute($m->{'id'}, freeze($m)) ;
 
@@ -54,9 +50,8 @@ sub cmd_catch {
     my $content = $msg->{'content'};
     my $replyto = '<@' . $author->{'id'} . '>';
     my $discord = $self->discord;
-    my $db      = Component::DBI->new();
 
-    my %catch = %{ $db->get('catch') };
+    my %catch = %{ $self->db->get('catch') };
 
     do "./Catch.pm";
     my @patterns = @Command::Catch::patterns;
@@ -75,7 +70,7 @@ sub cmd_catch {
             }
     
             $catch{$author->{'id'}} = time;
-            $db->set('catch', \%catch);
+            $self->db->set('catch', \%catch);
         
             if (ref $msg eq 'ARRAY') {
                 $msg = $msg->[rand @{ $msg }];
