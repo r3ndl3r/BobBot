934ef098a80aed43f859882c5e4a3afde8407d85
robertrendler <rendler@gmail.com>
2025-07-05 00:28:39 +1000

    Using global DB object

--- lib/Command/Paste.pm
+++ lib/Command/Paste.pm
@@ -6,7 +6,6 @@ use Moo;
 use strictures 2;
 use namespace::clean;
 
-use Component::DBI;
 use Storable qw(thaw);
 
 use Exporter qw(import);
@@ -15,16 +14,14 @@ our @EXPORT_OK = qw(cmd_paste);
 has bot                 => ( is => 'ro' );
 has discord             => ( is => 'lazy', builder => sub { shift->bot->discord } );
 has log                 => ( is => 'lazy', builder => sub { shift->bot->log } );
-
+has db                  => ( is => 'ro', required => 1 );
 has name                => ( is => 'ro', default => 'Paste' );
 has access              => ( is => 'ro', default => 1 );
 has description         => ( is => 'ro', default => 'Paste' );
 has pattern             => ( is => 'ro', default => '^paste ?' );
 has function            => ( is => 'ro', default => sub { \&cmd_paste } );
-has usage               => ( is => 'ro', default => <<EOF
-###
-EOF
-);
+has usage               => ( is => 'ro', default => '!paste <msgID' );
+
 
 sub cmd_paste {
     my ($self, $msg) = @_;
@@ -37,19 +34,16 @@ sub cmd_paste {
     my $args    = $msg->{'content'};
        $args    =~ s/$pattern//i;
 
-    if (my ($id) = $args =~ /^(\d+)$/) {
-        my $db  = Component::DBI->new();
-        my $dbh = $db->{'dbh'};
-        
+    if (my ($id) = $args =~ /^(\d+)$/) {        
         my $sql = "SELECT content FROM messages WHERE id = ?";
-        my $sth = $dbh->prepare($sql);
+        my $sth = $self->db->dbh->prepare($sql);
         $sth->execute($id);
 
         my $content = $sth->fetchrow_array();
         if ($content) {
             my $thaw = thaw($content);
             $discord->send_message($channel, $thaw);
-            $discord->create_reaction($msg->{'channel_id'}, $msg->{'id'}, "🤖");
+            $self->bot->react_robot($msg->{'channel_id'}, $msg->{'id'});
         }
     }
 }
